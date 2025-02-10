18387af5e5e6b7ac89159024c8bd0800fae983b4
Fernando Barbalho <fbarbalho@hotmail.com>
2025-02-10 17:27:36 -0300

    VErsão inicial

---
+++ analise_exploratoria.R
@@ -0,0 +1,56 @@
+library(colorspace)
+
+dados_trabalho_exportacao %>%
+  summarise(volume = sum(kg_liquido),
+            .by = co_ano) %>%
+  ggplot(aes(x= co_ano, y= volume)) +
+  geom_col() +
+  scale_x_continuous(breaks = 1997:2025) 
+
+
+dados_trabalho_exportacao %>%
+  summarise(valor = sum(vl_fob),
+            .by = co_ano) %>%
+  mutate(valor = valor/10^9) %>%
+  ggplot(aes(x= co_ano, y= valor)) +
+  geom_col(aes(fill= valor), show.legend = FALSE) +
+  geom_text(aes(x=co_ano, y= valor+0.1, label= round(valor,1)), size= 3.1) +
+  scale_x_continuous(breaks = 1997:2025) +
+  scale_fill_continuous_sequential(palette = "Heat 2") +
+  theme_light() +
+  theme(
+    panel.grid = element_blank()
+  ) +
+  labs(
+    title = "Exportação de aço para os EUA",
+    subtitle = "Valores em US$ bilhões",
+    x= "",
+    y="",
+    caption =  "Fonte: ComexStat. Elaboração própria"
+  )
+
+dados_trabalho_exportacao %>%
+  summarise(valor = sum(vl_fob),
+            .by = c(co_ano, no_sh2_por)) %>%
+  mutate(valor = valor/10^9) %>%
+  ggplot(aes(x= co_ano, y= valor, fill =  no_sh2_por)) +
+  geom_col() +
+  geom_text(aes(x=co_ano, y= valor+0.1, label= round(valor,1)), size= 3.1) +
+  scale_x_continuous(breaks = 1997:2025) +
+  scale_fill_discrete_qualitative(palette = "Dark 2") +
+  theme_light() +
+  theme(
+    panel.grid = element_blank(),
+    legend.position = "bottom"
+  ) +
+  labs(
+    title = "Exportação de aço para os EUA",
+    subtitle = "Valores em US$ bilhões",
+    x= "",
+    y="",
+    fill = "",
+    caption =  "Fonte: ComexStat. Elaboração própria"
+  )
+
+
+#Dimensões 1026 x 729
---
+++ etl.R
@@ -0,0 +1,84 @@
+library(tidyverse)
+library(data.table)
+
+
+ncm_sh <- read_delim("NCM_SH.csv", delim = ";", 
+                     escape_double = FALSE, locale = locale(encoding = "latin1"), 
+                     trim_ws = TRUE)
+
+ncm <- read_delim("NCM.csv", delim = ";", 
+                  escape_double = FALSE, locale = locale(encoding = "LATIN1"), 
+                  trim_ws = TRUE)
+
+exp_2025 <- read_delim("EXP_2025.csv", delim = ";", 
+                       escape_double = FALSE, trim_ws = TRUE)
+
+
+ncm_sh <- janitor::clean_names(ncm_sh)
+
+ncm<- janitor::clean_names(ncm)
+
+exp_2025 <- janitor::clean_names(exp_2025)
+
+
+ncm_sh$co_sh6 <-  as.factor(ncm_sh$co_sh6)
+
+teste<- "aqui tem pedaço"
+str_detect(teste, "\\baço\\b")
+
+
+# codigos_aco<- 
+#   (ncm_sh %>%
+#   filter(co_sh4 %in% c("7207")) %>%
+#   select(co_sh6))$co_sh6
+
+
+codigos_aco<- 
+  (ncm_sh %>%
+     filter(str_detect( no_sh4_por,"\\baço\\b"),
+            co_sh2 %in% c("72","73"))  %>%
+     select(co_sh6))$co_sh6
+
+
+codigos_aco_ncm<-
+  (ncm %>%
+  filter(co_sh6 %in% codigos_aco))$co_ncm
+
+
+exportacao_aco_eua_2025<-
+  exp_2025 %>%
+  filter(co_pais %in% c(249,396,873),
+         co_ncm %in% codigos_aco_ncm ) %>%
+  inner_join(
+    ncm %>%
+      select(co_ncm, co_sh6)
+  ) %>%
+  inner_join(
+    ncm_sh %>%
+      select(co_sh6, no_sh6_por, no_sh4_por, no_sh2_por )
+  )
+
+
+
+##Dados entre 1997 e 2025
+# data <- fread("exp_completa.csv")
+# data<- janitor::clean_names(data)
+
+dados_trabalho_exportacao<-
+  data %>%
+  filter(co_pais %in% c(249,396,873),
+         co_ncm %in% codigos_aco_ncm ) %>%
+  inner_join(
+    ncm %>%
+      mutate(co_ncm = as.numeric(co_ncm)) %>%
+      select(co_ncm, co_sh6)
+  ) %>%
+  inner_join(
+    ncm_sh %>%
+      select(co_sh6, no_sh6_por, no_sh4_por, no_sh2_por )
+  )
+
+glimpse(dados_trabalho_exportacao)
+
+
+names(dados_trabalho_exportacao)
